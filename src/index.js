1840c3bf0ed0515e96d203f71fde03936e63b5d5
Xinyang Zhang <xinyzhang9@gmail.com>
2019-11-22 22:41:48 +0800

    extract methods && inline variables && guardian

--- src/index.js
+++ src/index.js
@@ -8,33 +8,27 @@ class TennisGame {
     }
 
     getScore() {
-        let res;
-        if(this.score1 === this.score2) {
-            if(this.score1 < 3) {
-                res = this.scoreMap[this.score1] + ' ' + 'all';
-                return formatScore(res);
-            } else {
-                return "Deuce";
-            }
-        } else {
-            if(this.score1 < 4 && this.score2 < 4) {
-                res = this.scoreMap[this.score1] + ' ' + this.scoreMap[this.score2];
-                return formatScore(res);
-            } else {
-                if(this.score1 - this.score2 === 1) {
-                    return "Advantage " + this.player1;
-                }
-                if(this.score2 - this.score1 === 1) {
-                    return "Advantage " + this.player2;
-                }
-            }
-            if(this.score1 - this.score2 > 1) {
-                return this.player1 + ' '+'wins';
-            }
-            if(this.score2 - this.score1 > 1) {
-                return this.player2 + ' ' + 'wins';
-            }
-        }
+        if(this.hasWinner()) return this.getPlayerWithHigherScore() + ' ' + 'wins';
+        if(this.atDeuce()) return "Deuce";
+        if(this.atAll()) return formatScore(this.scoreMap[this.score1] + ' ' + 'all');
+        if(this.hasAdvantage()) return "Advantage " + this.getPlayerWithHigherScore();
+        return formatScore(this.scoreMap[this.score1] + ' ' + this.scoreMap[this.score2]);
+    }
+
+    hasWinner() {
+        return (this.score1 >=4 || this.score2 >=4) && Math.abs(this.score1 - this.score2) > 1;
+    }
+    getPlayerWithHigherScore() {
+        return this.score1 > this.score2 ? this.player1 : this.player2;
+    }
+    atDeuce() {
+        return this.score1 >=3 && this.score2 >=3 && this.score1 === this.score2;
+    }
+    atAll() {
+        return this.score1 < 3 && this.score2 < 3 && this.score1 === this.score2;
+    }
+    hasAdvantage() {
+        return this.score1 >= 3 && this.score2 >= 3 && Math.abs(this.score1 - this.score2) === 1;
     }
 
     player1Score() {
@@ -50,6 +44,5 @@ class TennisGame {
 const formatScore= (score) => {
     return score[0].toUpperCase()+score.slice(1).toLowerCase();
 }
-
 
 export default TennisGame;
